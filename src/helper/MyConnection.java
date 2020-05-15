b48e081f38370a22a58091323560ce5283aa7f1e
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-05-15 16:32:33 +0800

    Menampilkan Hasil Koneksi

--- src/helper/MyConnection.java
+++ src/helper/MyConnection.java
@@ -41,12 +41,28 @@ public class MyConnection {
         try {
             statement = con.createStatement();
             resultSet = statement.executeQuery(selectQuery);
-            while (resultSet.next()){
+            while(resultSet.next()){
                 matakuliah = new Matakuliah(
-
-                )
+                        resultSet.getInt("id_matakuliah"),
+                        resultSet.getString("nama_matakuliah"),
+                        resultSet.getString("singkatan_matakuliah"),
+                        resultSet.getString("nama_dosen"),
+                        resultSet.getString("kontak_dosen"),
+                        resultSet.getBoolean("aktif")
+                );
+                matakuliahList.add(matakuliah);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
+        for (Matakuliah value : matakuliahList) {
+            System.out.print(value.getIdMatakuliah());
+            System.out.print("\t");
+            System.out.print(value.getSingkatanMatakuliah());
+            System.out.print("\t");
+            System.out.print(value.getNamaDosen());
+            System.out.println();
         }
     }
 }
